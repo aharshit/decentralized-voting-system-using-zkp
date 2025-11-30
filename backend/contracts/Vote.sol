9242806107a0c113deb16f6d52e2c17515767292
Allagadda Harshit <aharshit2002@gmail.com>
2025-11-30 16:12:37 -0500

    add smart contract

---
+++ backend/contracts/Vote.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./Groth16verifier.sol"
+
+contract  Vote{
+    Groth16Verifier public verifier;
+
+    mapping(uint256 => bool) public usedNullifier;
+    mapping(uint256 => uint256) public voteCount;
+
+    constructor(address _verifier) {
+        verifier = Groth16Verifier(_verifier);
+    }
+
+    function vote(
+        uint256 candidate,
+        uint256[2] calldata a,
+        uint256[2][2] calldata b,
+        uint256[2] calldata c,
+        uint256[2] calldata publicSignals
+    ) external returns (bool) {
+        bool ok = verifier.verifyProof(a, b, c, publicSignals);
+        require(ok, "invalid proof");
+
+        uint256 commitment = publicSignals[0];
+        uint256 nullifier = publicSignals[1];
+
+        require(!usedNullifier[nullifier], "double vote");
+        usedNullifier[nullifier] = true;
+
+        voteCount[candidate] += 1;
+
+        return true;
+    }
+}
